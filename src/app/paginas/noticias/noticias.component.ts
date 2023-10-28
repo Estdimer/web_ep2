4b503e501585162ebb1c912a2f78b713bd89658d
Estdimer <115674299+Estdimer@users.noreply.github.com>
2023-10-28 12:49:21 -0300

    a

--- src/app/paginas/noticias/noticias.component.ts
+++ src/app/paginas/noticias/noticias.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as articulos from '../../../assets/data/arti.json';
+
+interface News {
+  titulo: string;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
   styleUrls: ['./noticias.component.css']
 })
-export class NoticiasComponent {
+export class NoticiasComponent implements OnInit {
+  newslist: News[] = articulos;
 
+  constructor() { }
+
+  ngOnInit() {
+    // Código de inicialización, si es necesario
+  }
 }
+
