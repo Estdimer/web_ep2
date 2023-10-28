c2f612845cecb72c8f8d1985f84908e6b90042b8
Estdimer <115674299+Estdimer@users.noreply.github.com>
2023-10-28 20:12:14 -0300

    aaa

--- src/app/paginas/noticias/noticias.component.ts
+++ src/app/paginas/noticias/noticias.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import articulos from '../../../assets/data/arti.json'
+
+interface News{
+  titulo:string;
+  descripcion:string
+}
 
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
   styleUrls: ['./noticias.component.css']
 })
-export class NoticiasComponent {
-  newsList: string[] = ['Pelicula 1', 'Pelicula 2', 'Pelicula 3'];
+export class NoticiasComponent implements OnInit{
+  
+  newsList: News[] = articulos;
+  constructor(){}
+  ngOnInit(): void {
+      throw new Error('Method not implemented.');
+    }
 }
-
 
