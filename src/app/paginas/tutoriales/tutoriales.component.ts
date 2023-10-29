9e790f9149f3b8b37fb2421f796e131ea754c896
DiegoAguileraZamora <116525150+DiegoAguileraZamora@users.noreply.github.com>
2023-10-29 17:40:00 -0300

    act

--- src/app/paginas/tutoriales/tutoriales.component.ts
+++ src/app/paginas/tutoriales/tutoriales.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import articulos from '../../../assets/data/tutorial.json'
 
+interface News{
+  imagen:string;
+  titulo:string;
+  descripcion:string;
+}
 @Component({
   selector: 'app-tutoriales',
   templateUrl: './tutoriales.component.html',
   styleUrls: ['./tutoriales.component.css']
 })
-export class TutorialesComponent {
-
+export class TutorialesComponent implements OnInit{
+  
+  newsList :News[] =articulos;
+  constructor(){}
+  ngOnInit(): void {
+      throw new Error('Method not implemented.');
+    }
 }
