b46e2c8f961b8b9da527099931aaf80a2f8d27e2
DiegoAguileraZamora <116525150+DiegoAguileraZamora@users.noreply.github.com>
2023-10-29 23:18:50 -0300

    busqueda

--- src/app/paginas/buscador/buscador.component.ts
+++ src/app/paginas/buscador/buscador.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
   styleUrls: ['./buscador.component.css']
 })
-export class BuscadorComponent {
+export class BuscadorComponent implements OnInit { 
+  buscarForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.buscarForm = fb.group({
+      buscar: ['']
+    });
+    console.log('funciono1');
+  }
+
+  ngOnInit() {
+    console.log('funciono1');
+  }
+
+  SaveData() {
+     //console.log(this.buscarForm.get('buscar').value);
+    console.log('funciono2');
+}
 }
